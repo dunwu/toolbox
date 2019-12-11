251d08b36ae49dc2606920137faf4f48b6d3dfd6
Zhang Peng <forbreak@163.com>
2019-12-11 11:48:57 +0800

    temp

--- toolbox-server/src/test/java/io/github/dunwu/toolbox/mail/service/MailServiceTest.java
+++ toolbox-server/src/test/java/io/github/dunwu/toolbox/mail/service/MailServiceTest.java
@@ -1,7 +1,7 @@
 package io.github.dunwu.toolbox.mail.service;
 
 import io.github.dunwu.toolbox.mail.dto.MailDTO;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
